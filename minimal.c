0f203acc98094003c538b01361b285f90a453e23
AnahitAmirkhanyan <anahit_amirkhanyan@edu.aua.am>
2017-10-06 10:21:57 +0400

    .

--- minimal.c
+++ minimal.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
-
+#include <unistd.h>
 int main()
 {
+	pid_t  child;
+	int p;
+	p=getpid();
+	printf("The main program process ID is %d\n", p);
+	child = fork();
 	printf("This is a minimal C program\n");
+	if(child != 0)
+		printf("This is the parent process.\n");
+	
+	else
+		printf("This is the child process.\n");
 	return 0;
 }
